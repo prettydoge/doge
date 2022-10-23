5a83f00dab6346d87f92ac688b22b720dbcd19f2
jianhua <jianhua.wang@poper.co>
2022-10-23 20:12:51 +0900

    Update main.go

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 	handle.OnPushEventAny(
 		func(deliveryID string, eventName string, e *github.PushEvent) error {
 			defer sugar.Sync()
-			sugar.Infow("received PushEvent:",
+			sugar.Infow("received PushEvent...",
 				"Repo", e.Repo.Name,
 				"RepoURL", e.Repo.URL,
 				"Action", e.Action,
